cbb6a992c56692b891f94f91edbc40bcc02ac947
Administrator <Administrator@BAL3EST1GTT20LJ>
2018-04-05 20:12:04 +0800

    1

--- src/main/java/com/zhiyou/flumeclientTest/forfanoutselector_20171107/SendPhoneNo.java
+++ src/main/java/com/zhiyou/flumeclientTest/forfanoutselector_20171107/SendPhoneNo.java
@@ -3,12 +3,12 @@ package com.zhiyou.flumeclientTest.forfanoutselector_20171107;
 
 import java.util.Random;
 
-import com.zhiyou.flumeclientTest.FlumeClient;
+//import com.zhiyou.flumeclientTest.FlumeClient;
 
 public class SendPhoneNo {
 
 	public static void main(String[] args){
-		FlumeClient flumeClient = new FlumeClient("master", 8888);
+//		FlumeClient flumeClient = new FlumeClient("master", 8888);
 		Random random = new Random();
 		for(int i=0;i<100;i++){
 			String phoneNo = "1"+random.nextInt(10)+random.nextInt(10)
@@ -20,9 +20,9 @@ public class SendPhoneNo {
 									+random.nextInt(10)
 									+random.nextInt(10)
 									+random.nextInt(10);
-			flumeClient.sendEvent(phoneNo);
+//			flumeClient.sendEvent(phoneNo);
 		}
-		flumeClient.close();
+//		flumeClient.close();
 	}
 	
 	
